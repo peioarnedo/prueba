b7a12cf201efc1e189efb08d2d7f5bac82e8edc9
peio <peioarnedo@gmail.com>
2018-02-26 14:00:48 +0100

    Añadido clase Git

---
+++ Git.java
@@ -0,0 +1,28 @@
+
+/**
+ *  class Git  
+ * 
+ * @author  
+ * @version  
+ */
+public class Git
+{
+     
+    private int x;
+
+    /**
+     * Constructor de la clase Git
+     */
+    public Git()
+    {
+     
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
